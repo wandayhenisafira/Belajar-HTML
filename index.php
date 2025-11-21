45b68cf321e66c8aa2020c92d0fba48717cd178d
wandayhenisafira <wandayhenisafira@gmail.com>
2025-11-21 15:52:11 +0700

    masih belajar

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+echo "Bilangan Ganjil 1 - 20:<br>";
+for ($i = 1; $i <= 20; $i++) {
+    if ($i % 2 != 0) {
+        echo $i . "<br>";
+    }
+}
+echo "<br>";
+
+echo "Bilangan Genap 1 - 20:<br>";
+for ($i = 1; $i <= 20; $i++) {
+    if ($i % 2 == 0) {
+        echo $i . "<br>";
+    }
+}
+echo "<br>";
+echo "Perulangan 100 - 0:<br>";
+for ($i = 100; $i >= 0; $i--) {
+  echo $i;
+  echo "<br>";
+}
+ echo "<br>";
+
+?>
+
